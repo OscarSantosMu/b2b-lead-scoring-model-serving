d0f6a3e1f05b1263916f94a26bb7a982cbab8c85
Oscar Santos <oscarsantosmu@gmail.com>
2025-11-24 15:27:18 -0600

    fix: ManagedEnvironmentInvalidNetworkConfiguration
    Provided subnet must have a size of at least /23

--- infra/terraform/modules/azure/main.tf
+++ infra/terraform/modules/azure/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "container_apps" {
   name                 = "container-apps-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/23"]
 }
 
 # Log Analytics Workspace
