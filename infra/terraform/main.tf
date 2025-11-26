8394cda43cccf720d1b3e288a0ab70ceb4316ae6
Oscar Santos <oscarsantosmu@gmail.com>
2025-11-26 01:09:12 -0600

    feat: make tf destroy everything

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -30,6 +30,9 @@ provider "azurerm" {
       # 2) On next apply, if a soft-deleted KV with same name exists → recover it instead of erroring
       recover_soft_deleted_key_vaults = true
     }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 
   skip_provider_registration = true
